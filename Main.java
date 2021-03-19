4c7e3c176191955c7714399e80f1d0620b486386
Ethan <777tabla@gmail.com>
2021-03-19 14:27:16 -0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,90 @@
+package Ethan.Kennedy.Chapter7.GradleJava.Project;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Main {
+
+	private JFrame frame;
+	private JTextField Numerator;
+	private JTextField Denominator;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		Numerator = new JTextField();
+		Numerator.setText("Numerator:");
+		Numerator.setBounds(10, 11, 86, 20);
+		frame.getContentPane().add(Numerator);
+		Numerator.setColumns(10);
+		
+		Denominator = new JTextField();
+		Denominator.setText("Denominator:");
+		Denominator.setBounds(10, 42, 86, 20);
+		frame.getContentPane().add(Denominator);
+		Denominator.setColumns(10);
+		
+		JLabel Label = new JLabel("Result:");
+		Label.setBounds(234, 30, 46, 14);
+		frame.getContentPane().add(Label);
+		
+		JButton btnNewButton = new JButton("Calculate");
+		
+		btnNewButton.setBounds(117, 26, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				double numerator = Double.parseDouble(Numerator.getText());
+				double denominator = Double.parseDouble(Denominator.getText());
+				
+				SimpleMath simpleMath = new SimpleMath();
+			
+				
+				
+				try {
+					Double result = simpleMath.divide(numerator, denominator);
+					Label.setText(result.toString());
+				} catch(ArithmeticException k) {
+					Label.setText("Cannot divide by 0");
+				}
+			
+			}
+		});
+	}
+}
---
+++ SimpleMath.java
@@ -0,0 +1,12 @@
+package Ethan.Kennedy.Chapter7.GradleJava.Project;
+
+public class SimpleMath {
+	double divide(double numerator, double denominator) {
+		if (denominator == 0) {
+			throw new ArithmeticException();
+		}
+		double result = numerator/denominator;
+		return result;
+	}
+
+}
